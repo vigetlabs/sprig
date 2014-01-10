c11c50360bee8481c99ad8428cc787106e58a65e
Ryan Foster <ryan.foster@viget.com>
2014-01-10 17:37:24 -0500

    Add feature spec for resetting sow configuration

--- spec/feature/configurations_spec.rb
+++ spec/feature/configurations_spec.rb
@@ -1,20 +1,30 @@
 require 'spec_helper'
 
 describe "Configurating Sow" do
-  let(:configuration) { Sow.configuration }
-
   before do
     stub_rails_root '~'
     stub_rails_env 'development'
   end
 
   it "can set the directory" do
-    configuration.directory.to_path.should == '~/db/seeds/development'
+    Sow.configuration.directory.to_path.should == '~/db/seeds/development'
 
     Sow.configure do |c|
       c.directory = 'seed_files'
     end
 
-    configuration.directory.to_path.should == '~/seed_files/development'
+    Sow.configuration.directory.to_path.should == '~/seed_files/development'
+  end
+
+  it "can reset the configuration" do
+    Sow.configure do |c|
+      c.directory = 'seed_files'
+    end
+
+    Sow.configuration.directory.to_path.should == '~/seed_files/development'
+
+    Sow.reset_configuration
+
+    Sow.configuration.directory.to_path.should == '~/db/seeds/development'
   end
 end
