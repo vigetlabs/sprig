066c7f4924ac6f3da74cb0adc82aa7cce849568a
Mike Ackerman <mikeackerman@gmail.com>
2014-01-22 16:38:30 -0700

    Lines up autloads

--- lib/sow/seed.rb
+++ lib/sow/seed.rb
@@ -1,9 +1,9 @@
 module Sow
   module Seed
-    autoload :Attribute, 'sow/seed/attribute'
-    autoload :AttributeCollection, 'sow/seed/attribute_collection'
-    autoload :Entry, 'sow/seed/entry'
-    autoload :Factory, 'sow/seed/factory'
-    autoload :Record, 'sow/seed/record'
+    autoload :Attribute,            'sow/seed/attribute'
+    autoload :AttributeCollection,  'sow/seed/attribute_collection'
+    autoload :Entry,                'sow/seed/entry'
+    autoload :Factory,              'sow/seed/factory'
+    autoload :Record,               'sow/seed/record'
   end
 end
