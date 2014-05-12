d9f636313976b27a1721c99148c2af7135b02b74
Ryan Foster <ryan.foster@viget.com>
2014-05-12 16:03:44 -0600

    Accept env arguments for sprig:install generator
    -PR #29
    -Default to ends with configuration files

--- lib/generators/sprig/install_generator.rb
+++ lib/generators/sprig/install_generator.rb
@@ -3,19 +3,28 @@ require 'rails/generators/base'
 module Sprig
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      argument :arg_envs, :type => :array, :optional => true
 
       desc "Install Sprig seed directories"
 
       def create_enviroment_directories
         empty_directory 'db/seeds'
 
-        environments.each { |env| empty_directory "db/seeds/#{env}" }
+        envs.each { |env| empty_directory "db/seeds/#{env}" }
       end
 
       private
 
-      def environments
-        [:development, :test, :integration, :staging, :production]
+      def envs
+        arg_envs ? arg_envs : default_envs
+      end
+
+      def default_envs
+        env_configs.map { |p| File.basename(p, File.extname(p)) }
+      end
+
+      def env_configs
+        Dir[Rails.root.join('config/environments', '*.rb')]
       end
     end
   end
