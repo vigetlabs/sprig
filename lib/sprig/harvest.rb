ded1083aa225feb204122ba294a1fb6d326e9e1f
h0tl33t <h0tl33t@gmail.com>
2014-06-02 13:25:32 -0400

    Extract Sprig::Harvest's configuration into its own file
    - Enable Sprig::Harvest to handle arguments from the command line (passing ENV via Rake task)

--- lib/sprig/harvest.rb
+++ lib/sprig/harvest.rb
@@ -1,14 +1,15 @@
 module Sprig
   module Harvest
-    autoload :Model,    'sprig/harvest/model'
-    autoload :Record,   'sprig/harvest/record'
-    autoload :SeedFile, 'sprig/harvest/seed_file'
+    autoload :Configuration, 'sprig/harvest/configuration'
+    autoload :Model,         'sprig/harvest/model'
+    autoload :Record,        'sprig/harvest/record'
+    autoload :SeedFile,      'sprig/harvest/seed_file'
 
     class << self
       def reap(options = {})
         configure do |config|
-          config.env     = options[:env] unless options[:env].nil?
-          config.classes = options[:classes] unless options[:classes].nil?
+          config.env     = options[:env]     || options['ENV']
+          config.classes = options[:classes] || options['CLASSES']
         end
 
         Model.all.each { |model| SeedFile.new(model).write }
@@ -26,38 +27,6 @@ module Sprig
 
       def configure
         yield configuration
-      end
-    end
-
-    private
-
-    class Configuration
-      VALID_CLASSES = ActiveRecord::Base.descendants
-
-      def env
-        @env ||= Rails.env
-      end
-
-      def env=(environment)
-        seeds_folder = Rails.root.join('db', 'seeds', environment)
-
-        FileUtils.mkdir_p(seeds_folder) unless File.directory? seeds_folder
-
-        @env = environment
-      end
-
-      def classes
-        @classes ||= VALID_CLASSES
-      end
-
-      def classes=(classes)
-        classes.each do |klass|
-          unless VALID_CLASSES.include? klass
-            raise ArgumentError, "Cannot create a seed file for #{klass} because it is not an ActiveRecord::Base-descendant."
-          end
-        end
-
-        @classes = classes
       end
     end
   end
--- spec/lib/sprig/harvest_spec.rb
+++ spec/lib/sprig/harvest_spec.rb
@@ -7,8 +7,6 @@ describe Sprig::Harvest do
     before do
       stub_rails_root
       stub_rails_env('dreamland')
-
-      ActiveRecord::Base.stub(:descendants).and_return([User, Post, Comment])
       Sprig::Harvest::SeedFile.stub(:new).and_return(seed_file)
     end
 
@@ -16,62 +14,40 @@ describe Sprig::Harvest do
       setup_seed_folder('./spec/fixtures/db/seeds/dreamland', &example)
     end
 
-    it "generates a seed file for each AR-backed model in the application" do
+    it "generates a seed file for each class" do
       seed_file.should_receive(:write).exactly(3).times
 
       subject.reap
     end
 
     context "when passed an environment in the options hash" do
-      context "and the seeds folder does not exist for the given environment" do
-        after do
-          FileUtils.remove_dir('./spec/fixtures/db/seeds/integration')
-        end
-
-        it "creates the seeds folder" do
+      context "in :env" do
+        it "sets the environment" do
           subject.reap(env: 'integration')
-
-          File.directory?('./spec/fixtures/db/seeds/integration').should == true
-        end
-
-        it "sets Harvest::Sprig's environment" do
-          subject.reap(env: 'integration')
-
           subject.env.should == 'integration'
         end
       end
 
-      context "and a seeds folder exists for the given environment" do
-        around do |example|
-          setup_seed_folder('./spec/fixtures/db/seeds/integration', &example)
-        end
-
-        it "sets Harvest::Sprig's environment" do
-          subject.reap(env: 'integration')
-
+      context "in 'ENV'" do
+        it "sets the environment" do
+          subject.reap('ENV' => ' Integration')
           subject.env.should == 'integration'
         end
       end
     end
 
     context "when passed a set of classes in the options hash" do
-      context "where one or more classes are not subclasses of ActiveRecord::Base" do
-        it "raises an error" do
-          expect {
-            subject.reap(classes: [Comment, Sprig::Harvest::Model])
-          }.to raise_error ArgumentError, 'Cannot create a seed file for Sprig::Harvest::Model because it is not an ActiveRecord::Base-descendant.'
+      context "in :classes" do
+        it "sets the classes" do
+          subject.reap(classes: [User, Post])
+          subject.classes.should == [User, Post]
         end
       end
 
-      context "where all classes are subclasses of ActiveRecord::Base" do
-        before do
-          Sprig::Harvest::Model.class_variable_set(:@@all, nil) # Reset cached @@all
-        end
-
-        it "generates a seed file for each of the given classes" do
-          seed_file.should_receive(:write).exactly(2).times
-
-          subject.reap(classes: [Comment, Post])
+      context "sets the classes" do
+        it "passes the value to its configuration" do
+          subject.reap('CLASSES' => 'User, Post')
+          subject.classes.should == [User, Post]
         end
       end
     end
