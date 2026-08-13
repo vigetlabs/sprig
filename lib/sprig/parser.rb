6696d4d391c56aad57d19e6af6bdc47651143181
mrsontag <matt.sontag@viget.com>
2026-08-13 18:56:56 -0400

    Remove retired Google API, update README formatting

--- lib/sprig/parser.rb
+++ lib/sprig/parser.rb
@@ -1,9 +1,8 @@
 module Sprig
   module Parser
-    autoload :Base,                  'sprig/parser/base'
-    autoload :Csv,                   'sprig/parser/csv'
-    autoload :Json,                  'sprig/parser/json'
-    autoload :Yml,                   'sprig/parser/yml'
-    autoload :GoogleSpreadsheetJson, 'sprig/parser/google_spreadsheet_json'
+    autoload :Base, 'sprig/parser/base'
+    autoload :Csv,  'sprig/parser/csv'
+    autoload :Json, 'sprig/parser/json'
+    autoload :Yml,  'sprig/parser/yml'
   end
 end
--- sprig.gemspec
+++ sprig.gemspec
@@ -22,8 +22,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "rspec"
   s.add_development_dependency "database_cleaner"
-  s.add_development_dependency "webmock"
-  s.add_development_dependency "vcr"
   s.add_development_dependency "test-unit"
   s.add_development_dependency "pry"
 
